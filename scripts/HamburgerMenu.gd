33f9f7c9f329bc77e55139d077736e1ed4b4beae
lorow <smykupyka@gmail.com>
2017-10-14 21:08:40 +0200

    probably fixed flickering while using menu

--- scripts/HamburgerMenu.gd
+++ scripts/HamburgerMenu.gd
@@ -24,27 +24,23 @@ func _ready():
 
 func _process(delta):
 	pos = get_pos()
-	
 	if move:
-		tween.interpolate_property(self,"rect/pos", pos, Vector2(actPos.x - (pos.x - startPos.x)/2, pos.y), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-		tween.start()
-		if direction == 1 and pos.x >= maxPos: resetPos(maxPos - 1, pos)
-		if direction == 2 and pos.x <= minPos: resetPos(minPos, pos)
+		if direction == 1 and pos.x < maxPos:
+			tween.interpolate_property(self,"rect/pos", pos, Vector2(actPos.x - (pos.x - startPos.x)/2, pos.y), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			tween.start()
+		if direction == 2 and pos.x > minPos:
+			tween.interpolate_property(self,"rect/pos", pos, Vector2(actPos.x - (pos.x - startPos.x)/2, pos.y), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			tween.start()
+			
+		if pos.x > maxPos + 1:
+			tween.stop(self, "rect/pos")
+			set_pos(Vector2(maxPos, pos.y))
+		if pos.x < minPos - 1:
+			tween.stop(self, "rect/pos")
+			set_pos(Vector2(minPos, pos.y))
+			
 		emit_signal("moveScreen", pos.x , timeTween)
-	else:
-		if direction == 1 and pos.x >= maxPos: set_pos(Vector2(maxPos - 1, pos.y))
-		if direction == 2 and pos.x <= minPos: set_pos(Vector2(minPos, pos.y))
-		emit_signal("moveScreen", pos.x , timeTween)
-	
-	if pos.x < minPos:
-		set_pos(Vector2(minPos, pos.y))
-		emit_signal("moveScreen", pos.x , timeTween)
-
-func resetPos(end, pos):
-	move = false
-	tween.stop(self, "rect/pos")
-	set_pos(Vector2(end, pos.y))
-
+			
 func _input(ev):
 	if ev.type == InputEvent.SCREEN_TOUCH and touchUtils.checkIfInside(ev, touchDetector.get_pos(), touchDetector.get_size()):
 		if ev.pressed:
