c7062a43f7752b316a6d735630e97138e02cb4e1
lorow <smykupyka@gmail.com>
2017-10-19 20:18:14 +0200

    added active texture changing functionality

--- scripts/buttonHandler.gd
+++ scripts/buttonHandler.gd
@@ -2,6 +2,7 @@ extends Control
 
 var buttons
 export var buttonsContainerNode = "VBoxContainer"
+onready var path_to_pressed_texture = load("res://sprites/homescreen/pomidor.tex")
 signal changeScreenTo
 
 func _ready():
@@ -9,7 +10,15 @@ func _ready():
 	for child in buttons:
 		# when user pressed a button we should tell the controler what screen we want to see next
 		child.connect("switchScreen", self, "sendIndex")
-
+		child.connect("switchScreen", self, "setActiveTexture")
+		
 # this will get called when the user presses a button in menu
+func setActiveTexture(index):
+	if not path_to_pressed_texture == null:
+		for child in buttons:
+			child.resetTexture()
+		buttons[index].set_normal_texture(path_to_pressed_texture)
+	else: print("jebać godot")
+
 func sendIndex(index):
 	emit_signal("changeScreenTo", index)
--- scripts/switchScreen.gd
+++ scripts/switchScreen.gd
@@ -1,10 +1,15 @@
 extends TextureButton
 
 export var screenIndexToShow = 0 # index of the screen to show after click
+onready var starterTexture = get_normal_texture()
 signal switchScreen
 
 func _ready():
 	connect("pressed", self, "change_screen")
+
+func resetTexture():
+	if not starterTexture == null:
+		self.set_normal_texture(starterTexture)
 
 func change_screen():
 	emit_signal("switchScreen", screenIndexToShow)
