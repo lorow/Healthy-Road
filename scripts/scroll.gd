4ae8fc958fe014fc98a0a407f721ad8a1b7bdf24
lorow <smykupyka@gmail.com>
2017-10-21 23:11:20 +0200

    add scroll system

---
+++ scripts/scroll.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var top = Vector2(0,0)
+var bottom = Vector2(0, get_size().y)
+var allowMove = false
+var touched
+var offset
+
+onready var contentNode = get_node("contentNode")
+onready var maxContentPos = get_size().y - contentNode.get_size().y
+onready var minContentPos = 0
+
+func _ready():
+	allowMove = checkIfBigger()
+	set_process_input(true)
+	
+func _input(ev):
+	if is_visible() and ((ev.type==InputEvent.SCREEN_TOUCH) or (ev.type==InputEvent.SCREEN_DRAG)) \
+	and touchUtils.checkIfInside(ev, get_pos(), get_size()) and allowMove:
+		if ev.type==InputEvent.SCREEN_TOUCH:
+			if ev.pressed:
+				offset = contentNode.get_pos().y - ev.pos.y
+				touched = true
+				
+		if ev.type==InputEvent.SCREEN_DRAG and touched:
+			var nextPos = Vector2(contentNode.get_pos().x, ev.pos.y + offset)
+			if nextPos.y <= minContentPos and nextPos.y >= maxContentPos:
+				contentNode.set_pos(nextPos)
+
+func checkIfBigger():
+	if get_size().y >= contentNode.get_size().y:
+		return false
+	return true
